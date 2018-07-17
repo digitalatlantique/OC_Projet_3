bd37802b550579eb8b94b0aa7415fa87ae668d7c
digitalatlantique <mick.info@yahoo.com>
2018-07-17 17:23:54 +0200

    Ajout du logger dans ControllerFactory

--- src/com/jeu/controller/ControllerJeuFactory.java
+++ src/com/jeu/controller/ControllerJeuFactory.java
@@ -1,11 +1,19 @@
 package com.jeu.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.jeu.model.Jeu;
 /**
  * Permet de retourner des instances de jeu : Combinaison+-, Mastermind
  * @author Workstation
  */
 public class ControllerJeuFactory {
+	
+    /**
+     * Permet la gestion des logs
+     */
+    private static Logger logger = LogManager.getLogger(ControllerJeuFactory.class);
 	
 	public static final int COMBINAISON = 1;
 	public static final int MASTERMIND = 2;
@@ -22,7 +30,10 @@ public class ControllerJeuFactory {
     			return new ControllerJeuMastermind();
     		}
     		
-    		default : return null;
+    		default : {
+    			logger.debug("Aucun jeu à charger !!");
+    			return null;
+    		}
     	}
     }
 
